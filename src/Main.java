fe1adf3448676553aad557e57eaf5b97b2c8aad6
Your Name <your.email@example.com>
2024-11-10 18:56:33 +0400

    pirveli davaleba

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+            long jami = 0;
+            for(int i = 10; i <= 100; i++) {
+                    if (i % 5 == 0) {
+                        jami = jami +i;
+                    }
+                if (i == 100) {
+                    System.out.println(jami);
+                }
+                }
+            }
+    }
+
