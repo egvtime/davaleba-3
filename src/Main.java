eda3fe157c65a15a422ff3da1055db5e4751c5ff
Your Name <your.email@example.com>
2024-11-10 20:16:22 +0400

    meore davaleba

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter number");
+            int number = scanner.nextInt();
 
-            long jami = 0;
-            for(int i = 10; i <= 100; i++) {
-                    if (i % 5 == 0) {
-                        jami = jami +i;
-                    }
-                if (i == 100) {
-                    System.out.println(jami);
+            for(int i = 1; i>0; i++) {
+                    if(number<(Math. pow(10, i))){
+                        System.out.println(i);
+                        break;
+                }
+                if(number==(Math. pow(10, i))){
+                    System.out.println(i+1);
+                    break;
                 }
                 }
             }
